a03708fb4ed8e9e02cc5275f5abe522a2ee0c033
Alex <Rallden@mail.ru>
2023-06-17 19:28:31 +0700

    2nd pack homework

---
+++ 3rd/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Enter number ");
+int num = int.Parse(Console.ReadLine());
+int num1 = ((num / 10) % 10);
+Console.WriteLine(num1);
---
+++ 5th/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number ");
+int num = int.Parse(Console.ReadLine());
+if (num > 5 && num < 8)
+Console.Write("Yes ");
+else
+Console.Write("No");
