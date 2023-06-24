10dbf3f093e0bcc27dba5c309c27fa6d3771f505
Alex <Rallden@mail.ru>
2023-06-25 02:35:22 +0700

    123

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 3");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"{max}");
---
+++ 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Enter number ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+
+{Console.Write("no 3rd number");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num /10;
+    }
+    {
+        num = num % 10;
+        }
+    Console.Write(num);
+}
