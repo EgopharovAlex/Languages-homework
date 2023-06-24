0c6eddc3ec0a3ff34ba7d40d94ff2fc0b36b9c38
Alex <Rallden@mail.ru>
2023-06-25 02:33:40 +0700

    4 homework

---
+++ 10/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNumbers(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result += n % 10;
+        n = n / 10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Enter numer N ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"{SumNumbers(n)} ");
---
+++ 9/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumDegree( int a, int b)
+{
+    int result = 1;
+    for(int i=0; i<b; i++)
+      result *= a;
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Enter numer A ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter numer B ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"number {a} in degree {b} = {NumDegree(a,b)} ");
